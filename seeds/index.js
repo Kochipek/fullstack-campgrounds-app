4f16e8daf0accc0bcd5fd3d046021785f00560a6
kochipek <ipekkochisarli@gmail.com>
2023-01-25 16:20:14 +0300

    edited

--- seeds/index.js
+++ seeds/index.js
@@ -16,12 +16,16 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 300; i++) {
+  for (let i = 0; i < 30; i++) {
     // we need to create a random number between 0 and 1000 because cities is an array of 1000 objects.
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 30) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image:`https://source.unsplash.com/random/600x400?camping,${i}`,
+      description: 'lorem ipsum dolor',
+      price: price,
     });
     await camp.save();
   }
