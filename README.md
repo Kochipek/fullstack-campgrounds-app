81c702e220c218978a756cba2d26254e90e04437
kochipek <ipekkochisarli@gmail.com>
2023-03-13 11:48:27 +0300

    controllers and star ratings

---
+++ README.md
@@ -0,0 +1,33 @@
+# fullstack-campgrounds-app
+<h3> To-do's  </h3> 
+
+
+- [X] Add Name, Image and Description to the campground.
+- [X] Create, Update, Delete the Campground.
+- [X] Add comments to campgrounds.
+- [X] Flash Important messages to warn or greet the users. 
+- [X] Responsive Web design.
+- [X] Authentication (Users can sign up or login using username and password && User can not submit campgrounds if they are not logged in.)
+- [X] Authorization
+      (User can only modify campgrounds created by them)
+- [ ] Cluster Map
+- [ ] Every registered user has profile where all his submitted campgrounds are shown
+- [ ] Search campground by name or location
+- [ ] Sort campgrounds by highest rating, most reviewed, lowest price, or highest price
+- [ ] Deploy
+
+<hr>
+<br>
+<h4> Tech in Use: </h4> 
+
+Frontend= JavaScript, EJS, HTML, CSS(Bootstrap 5) // Using BS5 for the first and probably the last time
+
+Backend= NodeJS, ExpressJS, MongoDB
+
+<h4> Libraries: </h4> 
+
+Password -for authentication
+
+JOI -for Validation
+
+Nodemon - monitor scripting
---
+++ controllers/campgrounds.js
@@ -0,0 +1,56 @@
+const campgroundDB = require('../models/campgroundDB');
+
+module.exports.index = async (req, res) => {
+    const campgrounds = await campgroundDB.find({});
+    res.render('campgrounds/index', { campgrounds })
+}
+
+module.exports.renderNewForm = (req, res) => {
+    res.render('campgrounds/newCampground');
+}
+
+module.exports.createCampground = async (req, res, next) => {
+    const campground = new campgroundDB(req.body.campground);
+    campground.author = req.user._id;
+    await campground.save();
+    req.flash('success', 'Successfully made a new campground!');
+    res.redirect(`/campgrounds/${campground._id}`)
+}
+
+module.exports.showCampground = async (req, res,) => {
+    const campground = await campgroundDB.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', { campground });
+}
+
+module.exports.renderEditForm = async (req, res) => {
+    const { id } = req.params;
+    const campground = await campgroundDB.findById(id)
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/edit', { campground });
+}
+
+module.exports.updateCampground = async (req, res) => {
+    const { id } = req.params;
+    const campground = await campgroundDB.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash('success', 'Successfully updated campground!');
+    res.redirect(`/campgrounds/${campground._id}`)
+}
+
+module.exports.deleteCampground = async (req, res) => {
+    const { id } = req.params;
+    await campgroundDB.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground')
+    res.redirect('/campgrounds');
+}
